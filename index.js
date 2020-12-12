f9a01c33ac90dee90154d0d628d7b05e9af1478c
divyansh_gupta_003 <guptadivyansh588@gmail.com>
2020-12-12 11:34:41 +0530

    commit1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = Process.env.PORT || 8000 ;
+
+const port = process.env.PORT || 8000 ;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 const path = require('path');
